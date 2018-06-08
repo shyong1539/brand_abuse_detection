8d9a6fc51ae63ba9087f6285274137b8afeb01f3
Emory Shyong <emoryshyong@Emorys-MacBook-Pro.local>
2018-06-08 09:48:50 -0700

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+  
 setup(
-  name = 'brand_abuse_detection',
-  packages = ['brand_abuse_detection'],  # this must be the same as the name above
-  version = '0.1',
-  description = 'Brand Abuse Detection',
-  author = 'Emory Shyong',
-  author_email = 'emory@clearedin.com',
-  url = 'https://github.com/shyong1539/brand_abuse_detection',
-  download_url = 'https://github.com/shyong1539/brand_abuse_detection/archive/0.1.tar.gz',
-  keywords = ['brand', 'abuse', 'detection'],
-  classifiers = [],
+   name='brand_abuse_detection',
+   version='0.1',
+   description='Brand Abuse Detection',
+   author='emoryshyong',
+   author_email='emory.shyong@gmail.com',
+   url='https://github.com/shyong1539/brand_abuse_detection',
+   license='Free',
+   packages=['brand_abuse_detection'],
 )
+
